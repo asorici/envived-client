fe7140222e82a5c4400a4741f192bbc91f8c8c7a
Irina Grigorescu <irinagry@gmail.com>
2013-08-29 17:15:35 +0300

    Added code to the MultiAutoCompleteTextView regarding the user's interests for suggestions to appear while the user is typing

--- src/com/envived/android/RegisterActivity.java
+++ src/com/envived/android/RegisterActivity.java
@@ -17,8 +17,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,38 +30,51 @@ import com.envived.android.api.exceptions.EnvSocialComException;
 import com.envived.android.api.exceptions.EnvSocialContentException;
 import com.envived.android.features.program.PresentationDetailsActivity;
 import com.envived.android.utils.ResponseHolder;
+import com.envived.android.utils.Utils;
 
-public class RegisterActivity extends SherlockActivity implements OnClickListener {
+public class RegisterActivity extends SherlockActivity implements
+		OnClickListener {
 	private static final String TAG = "RegisterActivity";
-	
+
 	private EditText mTxtEmail;
 	private EditText mTxtPassword;
 	private EditText mTxtFirst;
 	private EditText mTxtLast;
 	// private EditText mTxtAffiliation;
 	// private EditText mTxtInterests;
+	private MultiAutoCompleteTextView mAutocompleteSuggestTags;
 	private Button mBtnSubmit;
-	
+
 	private ProgressDialog mLoadingDialog;
-	
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
-        
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        
-        mTxtEmail = (EditText) findViewById(R.id.txt_email);
-        mTxtPassword = (EditText) findViewById(R.id.txt_password);
-        
-        mTxtFirst = (EditText) findViewById(R.id.txt_first);
-        mTxtLast = (EditText) findViewById(R.id.txt_last);
-        // mTxtAffiliation = (EditText) findViewById(R.id.txt_affiliation);
-        // mTxtInterests = (EditText) findViewById(R.id.txt_interests);
-        
-        
-        mBtnSubmit = (Button) findViewById(R.id.btn_submit);
-        mBtnSubmit.setOnClickListener(this);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+
+		getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+		mTxtEmail = (EditText) findViewById(R.id.txt_email);
+		mTxtPassword = (EditText) findViewById(R.id.txt_password);
+
+		mTxtFirst = (EditText) findViewById(R.id.txt_first);
+		mTxtLast = (EditText) findViewById(R.id.txt_last);
+		// mTxtAffiliation = (EditText) findViewById(R.id.txt_affiliation);
+		// mTxtInterests = (EditText) findViewById(R.id.txt_interests);
+
+		/*
+		 * SUGGESTIONS
+		 */
+		mAutocompleteSuggestTags = (MultiAutoCompleteTextView) this.findViewById(R.id.txt_interests);
+		ArrayAdapter<String> arrayTokens = new ArrayAdapter<String>(this,
+				android.R.layout.simple_dropdown_item_1line,
+				Utils.suggestedWords);
+		mAutocompleteSuggestTags.setAdapter(arrayTokens);
+		mAutocompleteSuggestTags.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+		Log.d(TAG, "CHOOSE INTERESTS REGISTER: " + mAutocompleteSuggestTags.getText().toString());
+
+		mBtnSubmit = (Button) findViewById(R.id.btn_submit);
+		mBtnSubmit.setOnClickListener(this);
 	}
 
 	public void onClick(View v) {
@@ -67,85 +82,91 @@ public class RegisterActivity extends SherlockActivity implements OnClickListene
 			new LoginTask().execute();
 		}
 	}
-	
+
 	private class LoginTask extends AsyncTask<Void, Void, ResponseHolder> {
 
 		private String mEmail;
 		private String mPassword;
 		private String mFirst;
 		private String mLast;
-		//private String mAffiliation;
-		//private String mInterests;
-		
+
+		// private String mAffiliation;
+		// private String mInterests;
+
 		@Override
 		protected void onPreExecute() {
-			mLoadingDialog = ProgressDialog.show(RegisterActivity.this, 
-					"", "Loading...", true);
+			mLoadingDialog = ProgressDialog.show(RegisterActivity.this, "",
+					"Loading...", true);
 			mEmail = mTxtEmail.getText().toString();
 			mPassword = mTxtPassword.getText().toString();
 			mFirst = mTxtFirst.getText().toString();
 			mLast = mTxtLast.getText().toString();
-			//mAffiliation = mTxtAffiliation.getText().toString();
-			//mInterests = mTxtInterests.getText().toString();
+			// mAffiliation = mTxtAffiliation.getText().toString();
+			// mInterests = mTxtInterests.getText().toString();
 		}
-		
+
 		@Override
-		protected ResponseHolder doInBackground(Void...args) {
-			
+		protected ResponseHolder doInBackground(Void... args) {
+
 			if (mFirst.equals("") || mLast.equals("")) {
 				return new ResponseHolder(HttpStatus.SC_NOT_ACCEPTABLE,
-						getResources().getString(R.string.msg_registration_details_required), null);
+						getResources().getString(
+								R.string.msg_registration_details_required),
+						null);
 			} else {
-				return ActionHandler.register(RegisterActivity.this,
-						mEmail, mPassword, mFirst, mLast);
+				return ActionHandler.register(RegisterActivity.this, mEmail,
+						mPassword, mFirst, mLast);
 			}
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		@Override
 		protected void onPostExecute(ResponseHolder holder) {
 			mLoadingDialog.cancel();
-			
+
 			if (!holder.hasError()) {
 				int statusCode = holder.getCode();
-			
+
 				if (statusCode == HttpStatus.SC_OK) {
-					startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+					startActivity(new Intent(RegisterActivity.this,
+							HomeActivity.class));
 					setResult(RESULT_OK);
 					finish();
 				} else if (statusCode == HttpStatus.SC_UNAUTHORIZED) {
-					Toast toast = Toast.makeText(RegisterActivity.this, 
+					Toast toast = Toast.makeText(RegisterActivity.this,
 							R.string.msg_unauthorized_login, Toast.LENGTH_LONG);
 					toast.show();
 				} else if (statusCode == HttpStatus.SC_NOT_ACCEPTABLE) {
-					Toast toast = Toast.makeText(RegisterActivity.this, 
-							R.string.msg_registration_details_required, Toast.LENGTH_LONG);
+					Toast toast = Toast.makeText(RegisterActivity.this,
+							R.string.msg_registration_details_required,
+							Toast.LENGTH_LONG);
 					toast.show();
-				}
-				else if (statusCode == HttpStatus.SC_BAD_REQUEST) {
+				} else if (statusCode == HttpStatus.SC_BAD_REQUEST) {
 					try {
-						Map<String, Object> errorDict = ResponseHolder.getActionErrorMessages(holder.getJsonContent());
-						String errorTitle = (String)errorDict.get("msg");
-						List<String> errorList = (List<String>)errorDict.get("errors");
-						
-						AlertDialog infoDialog = buildInfoDialog(errorTitle, errorList);
+						Map<String, Object> errorDict = ResponseHolder
+								.getActionErrorMessages(holder.getJsonContent());
+						String errorTitle = (String) errorDict.get("msg");
+						List<String> errorList = (List<String>) errorDict
+								.get("errors");
+
+						AlertDialog infoDialog = buildInfoDialog(errorTitle,
+								errorList);
 						infoDialog.show();
 					} catch (JSONException ex) {
 						Log.d(TAG, ex.getMessage());
-						Toast toast = Toast.makeText(RegisterActivity.this, 
-								R.string.msg_registration_details_check, Toast.LENGTH_LONG);
+						Toast toast = Toast.makeText(RegisterActivity.this,
+								R.string.msg_registration_details_check,
+								Toast.LENGTH_LONG);
 						toast.show();
 					}
-				}
-				else {
-					Toast toast = Toast.makeText(RegisterActivity.this, 
+				} else {
+					Toast toast = Toast.makeText(RegisterActivity.this,
 							R.string.msg_service_error, Toast.LENGTH_LONG);
 					toast.show();
 				}
-			}
-			else {
+			} else {
 				int msgId = R.string.msg_service_unavailable;
-				
+
 				try {
 					throw holder.getError();
 				} catch (EnvSocialComException e) {
@@ -158,41 +179,43 @@ public class RegisterActivity extends SherlockActivity implements OnClickListene
 					Log.d(TAG, e.toString(), e);
 					msgId = R.string.msg_service_error;
 				}
-				
-				Toast toast = Toast.makeText(RegisterActivity.this, msgId, Toast.LENGTH_LONG);
+
+				Toast toast = Toast.makeText(RegisterActivity.this, msgId,
+						Toast.LENGTH_LONG);
 				toast.show();
 			}
 		}
-		
+
 	}
-	
+
 	private AlertDialog buildInfoDialog(String title, List<String> messages) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		
+
 		LayoutInflater inflater = getLayoutInflater();
-		TextView titleDialogView = (TextView)inflater.inflate(R.layout.dialog_generic_title, null, false);
+		TextView titleDialogView = (TextView) inflater.inflate(
+				R.layout.dialog_generic_title, null, false);
 		titleDialogView.setText(title);
-		
-		
-		TextView bodyDialogView = (TextView)inflater.inflate(R.layout.dialog_generic_body, null, false);
+
+		TextView bodyDialogView = (TextView) inflater.inflate(
+				R.layout.dialog_generic_body, null, false);
 		String dialogMessage = "Errors: \n\n";
-		
+
 		for (String message : messages) {
 			dialogMessage += message + "\n\n";
 		}
-		
+
 		bodyDialogView.setText(dialogMessage);
-		
+
 		builder.setCustomTitle(titleDialogView);
 		builder.setView(bodyDialogView);
-		
+
 		builder.setPositiveButton("OK", new Dialog.OnClickListener() {
-		    @Override
-		    public void onClick(DialogInterface dialog, int which) { 
-		    	dialog.cancel();
-		    }
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
 		});
-		
+
 		return builder.create();
 	}
 }
