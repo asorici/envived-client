c150ed60f214379dda2535cc028f408d10798ea4
Alex Sorici <alex.sorici@gmail.com>
2013-08-30 18:18:32 +0300

    ignoring eclipse's .settings/ directory and doing local Url settings

--- src/com/envived/android/api/Url.java
+++ src/com/envived/android/api/Url.java
@@ -10,7 +10,8 @@ public class Url {
 	public static final String HTTPS = "https://";
 	//public static final String HOSTNAME = "envived.com:8800";
 	//public static final String HOSTNAME = "192.168.100.101:8080";
-	public static final String HOSTNAME = "192.168.1.102:8080";
+	//public static final String HOSTNAME = "192.168.1.102:8080";
+	public static final String HOSTNAME = "192.168.1.4:8080";
 	
 	public static final String BASE_URL = "/envived/client/v2/";
 	public static final String ACTION_RELATIVE_URL = BASE_URL + "actions/";
