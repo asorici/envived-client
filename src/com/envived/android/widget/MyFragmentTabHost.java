62100108c104328a67b410880de3f3b19f9eb784
Alex Sorici <alex.sorici@gmail.com>
2014-01-13 10:29:56 +0100

    Small updates

--- src/com/envived/android/widget/MyFragmentTabHost.java
+++ src/com/envived/android/widget/MyFragmentTabHost.java
@@ -2,14 +2,14 @@ package com.envived.android.widget;
 
 import java.util.ArrayList;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
