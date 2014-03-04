ed4116684a285f6335aa99250cedea7769a66886
Alex Tomescu <alex.dan.tomescu@gmail.com>
2014-03-04 18:12:20 +0200

    added comments, removed unused and unwanted class and fixed the URL issue

--- src/com/envived/android/utils/EnvivedMessageService.java
+++ src/com/envived/android/utils/EnvivedMessageService.java
@@ -13,14 +13,27 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.envived.android.api.Url;
+
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+/**
+ * EnvivedMessageService runs as a service in the background, not visible to the user. It implements the long polling mechanism
+ * by polling the server for minutes at a time. If no message is received, then it checks if the service was supposed to stop,
+ * and if not it polls again. In case a message is received from the server, it checks if the message type of the message and
+ * send the content by broadcast to the receivers. Only the receivers which work with that type of message will get it.
+ * The types of messages are: envived_app_update, envived_app_message and envived_event. 
+ * 
+ * @author alextomescu
+ *
+ */
 public class EnvivedMessageService extends IntentService {
 	private static final String TAG = "EnvivedMessageService";
-	public static final String LONG_POLL_URL = "http://192.168.1.192:8080/envived/client/notifications/me/";
-	public static final int MSG_TIMEOUT_MILLIS = 60000;
+	private static final String URL_BASE = Url.HTTP + Url.HOSTNAME;
+	public static final String LONG_POLL_URL = URL_BASE + "/envived/client/notifications/me/";
+	public static final int MSG_TIMEOUT_MILLIS = 100000;
 	private boolean stopFlag = false;
 	
 	private HttpGet mGetRequest;
@@ -35,16 +48,20 @@ public class EnvivedMessageService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent arg) {
+		// set up the connection to the server
 		DefaultHttpClient retrieveMsgClient = new DefaultHttpClient();
 	    HttpParams httpParameters = new BasicHttpParams();
 	    HttpConnectionParams.setSoTimeout(httpParameters, MSG_TIMEOUT_MILLIS);
 	    retrieveMsgClient.setParams(httpParameters);
 	    mGetRequest = new HttpGet(LONG_POLL_URL);
 	    HttpResponse response;
+	    
+	    // while the service is not stopped keep polling the server
 	    while(!stopFlag) {
 			try {
 				mGetRequest = new HttpGet(LONG_POLL_URL);
 				
+				// retrieve a session id from the program and add it to the header of the request for authentication.
 				String sessionCookie = Preferences.getStringPreference(EnvivedMessageService.this, com.envived.android.api.AppClient.SESSIONID);
 				if (sessionCookie != null) {
 					mGetRequest.setHeader("Cookie", sessionCookie);
@@ -54,6 +71,7 @@ public class EnvivedMessageService extends IntentService {
 				ResponseHolder holder = ResponseHolder.parseResponse(response);
 				JSONArray messages = holder.getJsonContent().getJSONObject("data").getJSONArray("messages");
 				
+				// extract the content from all the messages and send it to the appropriate receiver.
 				for (int i = 0; i < messages.length(); i++) {
 					JSONObject jsonMsg = new JSONObject(messages.getString(i));
 		        	String msgData = jsonMsg.getString("data");
