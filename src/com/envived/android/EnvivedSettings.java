ea4334372963906528e9ca4e035af3409bd21539
Alex Tomescu <alex.dan.tomescu@gmail.com>
2014-03-04 20:06:57 +0200

    removed gcm from the preference name and added a way to toggle notifications on and off

--- src/com/envived/android/EnvivedSettings.java
+++ src/com/envived/android/EnvivedSettings.java
@@ -1,13 +1,18 @@
 package com.envived.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
+import android.util.Log;
+
+import com.envived.android.utils.EnvivedMessageService;
 
 public class EnvivedSettings extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	public static final String KEY_ENVIVED_NOTIFICATIONS = "envived_notifications";
+	public static final String TAG = "EnvivedSettings";
     
     private CheckBoxPreference mEnvivedNotificationsPreference;
     
@@ -25,7 +30,32 @@ public class EnvivedSettings extends PreferenceActivity implements OnSharedPrefe
 
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		// TODO Auto-generated method stub
-		
+		if (key.equals(KEY_ENVIVED_NOTIFICATIONS)) {
+			if (!sharedPreferences.getBoolean(KEY_ENVIVED_NOTIFICATIONS, true)) {
+				// the user has deactivated notifications
+				Log.d(TAG, "stopping service");
+				stopService(new Intent(EnvivedSettings.this, EnvivedMessageService.class));
+			} else {
+				// the user has activated notifications
+				Log.d(TAG, "starting service");
+				startService(new Intent(EnvivedSettings.this, EnvivedMessageService.class));
+			}
+		}
+	}
+	
+	@Override
+	protected void onResume() {
+	    super.onResume();
+	    // Set up a listener whenever a key changes
+	    getPreferenceScreen().getSharedPreferences()
+	            .registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	protected void onPause() {
+	    super.onPause();
+	    // Unregister the listener whenever a key changes
+	    getPreferenceScreen().getSharedPreferences()
+	            .unregisterOnSharedPreferenceChangeListener(this);
 	}
 }
