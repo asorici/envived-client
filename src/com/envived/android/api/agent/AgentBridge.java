9646765e264bc5f93dbfa97d9ede9dc255880e52
Alex Tomescu <alex.dan.tomescu@gmail.com>
2014-05-16 10:34:16 +0300

    wip

--- src/com/envived/android/api/agent/AgentBridge.java
+++ src/com/envived/android/api/agent/AgentBridge.java
@@ -13,7 +13,9 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Build;
 
 import com.envived.android.utils.EnvivedEvent;
